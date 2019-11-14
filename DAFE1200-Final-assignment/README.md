855867de227738b4e6072a3ea7849558f77d973e
tomhbasmo <47509183+tomhbasmo@users.noreply.github.com>
2019-11-14 01:46:51 +0100

    controls + back to top + """responsive""" nav

---
+++ DAFE1200-Final-assignment/README.md
@@ -0,0 +1,15 @@
+# DAFE1200-Final-assignment
+
+  Innledning/Ide ikke bestemt enda.
+
+## Gruppemedlemmer
+
+  - Jørund Topp Løvlien
+  - Nikola Dordevic
+  - Hajin Barzingi
+  - Nina Hübertz Emaus
+  - Ola Gynnild Berg
+  - Tom Basmo
+  - Rosa Berg
+
+
---
+++ DAFE1200-Final-assignment/js/main.js
@@ -0,0 +1,12 @@
+
+
+
+/*
+document.getElementById('backToTop').addEventListener('click', function(e) {
+    e.preventDefault();
+    var target = document.getElementById('controls');
+    target.setAttribute('tabindex', '-1');
+    target.focus();
+  });
+
+  */
