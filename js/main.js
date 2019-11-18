ed243719bb500a22349ffe3ebed7e9dd4a3ca409
Jørund Topp Løvlien <noen54@gmail.com>
2019-11-18 15:50:43 +0100

    localStorage textSize

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+var textSize = localStorage.getItem("textSize");
+var htmlCss = document.getElementsByTagName("html");
+if(textSize == null){
+    localStorage.setItem("textSize", "medium");
+}
+else if(textSize == "medium"){
+    htmlCss[0].style.fontSize = "medium";
+}
+else if(textSize == "large"){
+    htmlCss[0].style.fontSize = "large";
+}
+else if(textSize == "x-large"){
+   htmlCss[0].style.fontSize = "x-large";
+}
+
 var evtTextSize = document.querySelectorAll(".textSizeValg");
 for(var i = 0; i < evtTextSize.length; i++){
     var self = evtTextSize[i];
@@ -11,14 +26,16 @@ for(var i = 0; i < evtTextSize.length; i++){
 }
 
 function endreTextSize(n) {
-    var htmlCss = document.getElementsByTagName("html");
     if(n == 0){
         htmlCss[0].style.fontSize = "medium";
+    localStorage.setItem("textSize", "medium");
     }
     else if(n == 1){
         htmlCss[0].style.fontSize = "large";
+    localStorage.setItem("textSize", "large");
     }
     else if(n == 2){
         htmlCss[0].style.fontSize = "x-large";
+    localStorage.setItem("textSize", "x-large");
     }
 }
