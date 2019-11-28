9c7e1de4002317999f810e62223f1facdaabcd04
tomhbasmo <47509183+tomhbasmo@users.noreply.github.com>
2019-11-28 19:21:20 +0100

    keyboard accessibility
    endra fallback for textsize til å være en egen side fordi den må være mulig å komme til uten hover.
    Har gjort det mulig å åpne textsize dropdown med enter - men må endre alle liste-elementene inni til å være button ..
    
    NOEN BUGS da...

--- js/main.js
+++ js/main.js
@@ -95,6 +95,17 @@ topLevelLink.forEach(link => {
   })
 */
 
+/*
+var textSizeButton = document.querySelectorAll('textSizeDrop');
+
+function toggleTextSize(){
+      
+            console.log("gahhh");
+            textSizeButton.setAttribute('aria-expanded', "true");
+}
+ */
+
+
 function hvisJShide() {
     document.getElementById("textSizeNoJS").style.display = "none";
     document.getElementById("textSizeNoJS").style.visibility = "hidden";
@@ -106,3 +117,31 @@ function hvisJShide() {
 
 }
 window.onload = hvisJShide;
+
+
+
+
+
+
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
