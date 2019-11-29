413fa9e3dee695db15a5f6d8f6fb616bca72fa4c
Jørund Topp Løvlien <noen54@gmail.com>
2019-11-29 16:53:05 +0100

    Merge textsize

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 var textSize = localStorage.getItem("textSize");
 var htmlCss = document.getElementsByTagName("html");
+var textSizeDrop = document.getElementsByClassName("textSizeDrop")[0];
+var textSize = document.getElementById("textSize");
+var dropBtn = document.getElementById("dropBtn");
+var btnAccept = document.getElementById("btnAccept");
+var cookiesBox = document.getElementById("cookiesBox");
 var valg1 = document.getElementById("size1");
 var valg2 = document.getElementById("size2");
 var valg3 = document.getElementById("size3");
+valg1.style.backgroundColor = "#C2C5CC";
 function sjekkSize(){
     if(textSize == null){
         localStorage.setItem("textSize", "medium");
-        valg1.style.backgroundColor = "#DDCBDD";
-        valg2.style.backgroundColor = "#FFEDFF";
-        valg3.style.backgroundColor = "#FFEDFF";
+        valg1.style.backgroundColor = "#C2C5CC";
+        valg2.style.backgroundColor = "#FFFFFF";
+        valg3.style.backgroundColor = "#FFFFFF";
     }
     else if(textSize == "medium"){
         htmlCss[0].style.fontSize = "medium";
-        valg1.style.backgroundColor = "#DDCBDD";
-        valg2.style.backgroundColor = "#FFEDFF";
-        valg3.style.backgroundColor = "#FFEDFF";
+        valg1.style.backgroundColor = "#C2C5CC";
+        valg2.style.backgroundColor = "#FFFFFF";
+        valg3.style.backgroundColor = "#FFFFFF";
     }
     else if(textSize == "large"){
         htmlCss[0].style.fontSize = "large";
-        valg1.style.backgroundColor = "#FFEDFF";
-        valg2.style.backgroundColor = "#DDCBDD";
-        valg3.style.backgroundColor = "#FFEDFF";
+        valg1.style.backgroundColor = "#FFFFFF";
+        valg2.style.backgroundColor = "#C2C5CC";
+        valg3.style.backgroundColor = "#FFFFFF";
     }
     else if(textSize == "x-large"){
         htmlCss[0].style.fontSize = "x-large";
-        valg1.style.backgroundColor = "#FFEDFF";
-        valg2.style.backgroundColor = "#FFEDFF";
-        valg3.style.backgroundColor = "#DDCBDD";
+        valg1.style.backgroundColor = "#FFFFFF";
+        valg2.style.backgroundColor = "#FFFFFF";
+        valg3.style.backgroundColor = "#C2C5CC";
     }
-}sjekkSize();
+}
 
 var evtTextSize = document.querySelectorAll(".textSizeValg");
 for(var i = 0; i < evtTextSize.length; i++){
@@ -42,30 +48,74 @@ for(var i = 0; i < evtTextSize.length; i++){
     })
 }
 
+for(var i = 0; i < evtTextSize.length; i++){
+    var self = evtTextSize[i];
+    
+    self.addEventListener("keypress", function(e) {
+        event.preventDefault();
+        if(event.keyCode == 13){
+            var textValg = e.target;
+            var valgIndeks = textValg.getAttribute("data-valg");
+            endreTextSize(valgIndeks);
+            dropBtn.blur();
+            textSizeDrop.style.display = "none";
+        }
+    })
+}
+
 function endreTextSize(n) {
     if(n == 0){
         htmlCss[0].style.fontSize = "medium";
         localStorage.setItem("textSize", "medium");
-        valg1.style.backgroundColor = "#DDCBDD";
-        valg2.style.backgroundColor = "#FFEDFF";
-        valg3.style.backgroundColor = "#FFEDFF";
+        valg1.style.backgroundColor = "#C2C5CC";
+        valg2.style.backgroundColor = "#FFFFFF";
+        valg3.style.backgroundColor = "#FFFFFF";
     }
     else if(n == 1){
         htmlCss[0].style.fontSize = "large";
         localStorage.setItem("textSize", "large");
-        valg1.style.backgroundColor = "#FFEDFF";
-        valg2.style.backgroundColor = "#DDCBDD";
-        valg3.style.backgroundColor = "#FFEDFF";
+        valg1.style.backgroundColor = "#FFFFFF";
+        valg2.style.backgroundColor = "#C2C5CC";
+        valg3.style.backgroundColor = "#FFFFFF";
     }
     else if(n == 2){
         htmlCss[0].style.fontSize = "x-large";
         localStorage.setItem("textSize", "x-large");
-        valg1.style.backgroundColor = "#FFEDFF";
-        valg2.style.backgroundColor = "#FFEDFF";
-        valg3.style.backgroundColor = "#DDCBDD";
+        valg1.style.backgroundColor = "#FFFFFF";
+        valg2.style.backgroundColor = "#FFFFFF";
+        valg3.style.backgroundColor = "#C2C5CC";
     }
 }
 
+dropBtn.onkeypress = function() {
+    if(event.keyCode == 13){
+        textSizeDrop.style.display = "block";
+    }
+}
+
+btnAccept.onclick = function(){
+    cookiesBox.style.display = "none";
+    localStorage.setItem("cookies", "ja");
+}
+
+function sjekkCookies(){
+    var cookies = localStorage.getItem("cookies");
+    if(cookies == "ja"){
+        cookiesBox.style.display = "none";
+        sjekkSize();
+    }
+    else{
+        cookiesBox.style.display = "block";
+    }
+} sjekkCookies();
+
+textSize.onmouseover = function (){
+    textSizeDrop.style.display = "block";
+}
+
+textSize.onmouseout = function (){
+    textSizeDrop.style.display = "none";
+}
 
 //display: none;
 //visibility: hidden;
@@ -95,17 +145,6 @@ topLevelLink.forEach(link => {
   })
 */
 
-/*
-var textSizeButton = document.querySelectorAll('textSizeDrop');
-
-function toggleTextSize(){
-      
-            console.log("gahhh");
-            textSizeButton.setAttribute('aria-expanded', "true");
-}
- */
-
-
 function hvisJShide() {
     document.getElementById("textSizeNoJS").style.display = "none";
     document.getElementById("textSizeNoJS").style.visibility = "hidden";
@@ -121,56 +160,3 @@ window.onload = hvisJShide;
 
 
 
-
-/* skru av og på classes */
-function textSizeDropDown() {
-    document.getElementById("textSizeDropDown").classList.toggle("show");
-    var x = document.getElementById("dropbtn").getAttribute("aria-expanded"); 
-    if (x == "true") 
-    {
-    x = "false"
-    } else {
-    x = "true"
-    }
-    document.getElementById("dropbtn").setAttribute("aria-expanded", x);
-    document.getElementById("textSize").setAttribute("aria-expanded", x);
-  }
-  
-  document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    var dropdowns = document.getElementById("textSizeDropDown");
-    if (evt.keyCode == 27) {
-       
-       /* if (dropdowns.classList.contains('show')) {
-          dropdowns.classList.remove('show');
-        } */
-        alert('Esc key pressed.');
-    }
-};
-
-
-
-  // PRØVER Å FJERNE FOKUS NÅR DEN TABBES VIDERE Burde legge til escape key her - eller en egen function ??
-  document.getElementById('textSizeDropDown').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.getElementById("textSizeDropDown").setAttribute('aria-expanded', "true");
-    var target = document.getElementById('size1');
-    target.setAttribute('tabindex', '-1');
-    target.focus();
-  });
-
-
-
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
