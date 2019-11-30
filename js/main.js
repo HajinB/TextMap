ca0a4d74216e3560e91264e1c8aa24ef86e02909
Jørund Topp Løvlien <noen54@gmail.com>
2019-11-30 13:20:01 +0100

    js  fix

--- js/main.js
+++ js/main.js
@@ -118,13 +118,6 @@ btnAccept.onclick = function(){
     localStorage.setItem("cookies", "ja");
 }
 
-document.onclick = function(event) {
-    if(event.target != dropBtn && aapenTextSize){
-        aapenTextSize = Boolean(false);
-        textSizeDrop.style.display = "none";
-        dropBtn.setAttribute('aria-expanded', false);
-    }
-}
 
 function sjekkCookies(){
     var cookies = localStorage.getItem("cookies");
@@ -149,6 +142,13 @@ textSize.onmouseout = function (){
     dropBtn.setAttribute('aria-expanded', false);
 }
 
+document.onclick = function(event) {
+    if((event.target != dropBtn || event.target != dropBtn.childNodes) && aapenTextSize){
+        aapenTextSize = Boolean(false);
+        textSizeDrop.style.display = "none";
+        dropBtn.setAttribute('aria-expanded', false);
+    }
+}
 //display: none;
 //visibility: hidden;
 //var el = document.getElementById('#foo');
